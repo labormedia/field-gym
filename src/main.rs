781c8b0357193cbfc308023aaa0ced053545baca
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-10-17 03:22:58 +0000

    Change name type reference.

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,16 @@ fn main() {
     let a = BabyBearElem::random(&mut thread_rng());
     let b = BabyBearElem::random(&mut thread_rng());
     
-    let a_u32 = a.as_u32().try_into().unwrap();
-    let b_u32 = b.as_u32().try_into().unwrap();
+    let a_usize = a.as_u32().try_into().unwrap();
+    let b_usize = b.as_u32().try_into().unwrap();
     
     let g = BabyBearElem::new(7);
     
     // g to the power of a_u32*b_u32.
-    let g_ab = g.pow(a_u32*b_u32);
+    let g_ab = g.pow(a_usize*b_usize);
     
     // g to the power of a, to the power of b.
-    let g_a_b = g.pow(a_u32).pow(b_u32);
+    let g_a_b = g.pow(a_usize).pow(b_usize);
     
     assert_eq!(g_ab, g_a_b);
 }
