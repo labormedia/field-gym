bb5f5b6f3f013df69c1080c52d9a969e19c0f326
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-10-17 15:16:52 +0000

    Adds assertion

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,14 @@ fn main() {
     let g = BabyBearElem::new(7);
     
     // g to the power of a_u32*b_u32.
-    let g_ab = g.pow(a_usize*b_usize);
+    let g_ab_usize = g.pow(a_usize*b_usize);
+    
+    // g to the power of a*b
+    let g_ab = g.pow((a*b).as_u32().try_into().unwrap());
     
     // g to the power of a, to the power of b.
     let g_a_b = g.pow(a_usize).pow(b_usize);
     
-    assert_eq!(g_ab, g_a_b);
+    assert_eq!(g_ab_usize, g_a_b);
+    assert_ne!(g_ab, g_a_b);
 }
